04513f828c69b422c46b4de2b5888a7413c04db4
Allenary <kochetovanina@gmail.com>
2014-03-06 15:16:08 +0200

    unused classes deleted

--- HbSelenTest/Bootstrap/BootstrapElements/IElementsFactory.java
+++
@@ -1,9 +0,0 @@
-package BootstrapElements;
-
-import java.util.List;
-
-import org.openqa.selenium.WebElement;
-
-public interface IElementsFactory {
-	<E extends IElement> List<E> create(Class<E> ElementClass, WebElement element);
-}
