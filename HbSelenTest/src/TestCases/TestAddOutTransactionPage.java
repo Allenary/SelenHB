f4a601f8f8fcca6cc32c39cc051478ac2266ce4a
Allenary <kochetovanina@gmail.com>
2014-03-18 16:21:40 +0200

    create test in testng  style

--- HbSelenTest/src/TestCases/TestAddOutTransactionPage.java
+++ HbSelenTest/src/TestCases/TestAddOutTransactionPage.java
@@ -3,34 +3,56 @@ package TestCases;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import Pages.PageAdd;
 
 public class TestAddOutTransactionPage {
-	 PageAdd page;
-	
-	@Before
-	public void initPage(){
-		WebDriver w;
+    static PageAdd page;
+    static WebDriver w;
+    
+	@BeforeClass
+	public static void initPage(){
+		
 		w = new FirefoxDriver();
 		w.get("http://fortest.resscode.org.ua/hb/outtransactions/add");
 		page = new PageAdd();
 		page.init(w);
 	}
-	@Test
+	
+	@Test(enabled = false)
 	public void verifyTitle() {
 		assertEquals("Wrong Title", page.getTitle(), page.getExpectedPageTitle());
 	}
-	@Test
-	public void saveTransactionWithData(){
-		page.setName("test");
-		page.selectFirstOptionInAllCombos();
+	@DataProvider
+	private Object[][] getTestData() {
+        return new Object[][]{
+        		{"qtd1"},
+        		{"qtd2"},
+        		{"qtd3"},
+        		{"qtd4"}
+        };
+    }
+	
+	@Test(dataProvider = "getTestData")
+	public void saveTransactionWithData(String testValue){
+		page.setName(testValue);
+		//page.selectFirstOptionInAllCombos();
 		page.save();
 		assertTrue(page.getModalMessage().contains("Your data has been successfully stored into the database"));
 	}
-
+	
+	/*
+	 * hmm.. doesn't close browser... something wrong
+	 */
+	@AfterClass
+	public static void postSteps(){
+		w.close();
+	}
+	
 }
